9cbd269de3ab738e55e62ab7b41cfa2b80da0372
zakaria <oulaminzakaria@gmail.com>
2026-04-20 09:02:55 +0100

    Fix: Force HTTPS on production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // نفرضو HTTPS إيلا كنا خدامين فالسيرڤور الحقيقي
+        if (config('app.env') === 'production') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
